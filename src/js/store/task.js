69c9cda4f02fe8a1e45d3a54d061559d1b8e63d3
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-06-29 16:18:17 +0300

    refactoring: Now new tasks adding to the top of the tasks list

--- src/js/store/task.js
+++ src/js/store/task.js
@@ -2,45 +2,14 @@ import { makeAutoObservable } from "mobx";
 import { v4 } from "uuid";
 
 class Task {
-  tasks = [
-    {
-      id: v4(),
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adip dolor sit amet dolor sit amet dolor sit amet dolor sit amet",
-      completed: false,
-    },
-    {
-      id: v4(),
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adip dolor sit amet dolor sit amet dolor sit amet dolor sit amet",
-      completed: false,
-    },
-    {
-      id: v4(),
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adip dolor sit amet dolor sit amet dolor sit amet dolor sit amet",
-      completed: true,
-    },
-    {
-      id: v4(),
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adip dolor sit amet dolor sit amet dolor sit amet dolor sit amet",
-      completed: false,
-    },
-    {
-      id: v4(),
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adip dolor sit amet dolor sit amet dolor sit amet dolor sit amet",
-      completed: true,
-    },
-  ];
+  tasks = [];
 
   constructor() {
     makeAutoObservable(this);
   }
 
   addTask(title, completed = false) {
-    this.tasks.push({
+    this.tasks.unshift({
       id: v4(),
       title,
       completed,
